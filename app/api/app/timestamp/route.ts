70e1c13ec8f4bc067022ea26325c73635157e8a8
daws-4 <villamizarandresdavid@gmail.com>
2025-03-26 17:01:26 -0400

    26-03-2025

--- app/api/app/timestamp/route.ts
+++ app/api/app/timestamp/route.ts
@@ -8,6 +8,17 @@ connectDB();
 export async function POST(request: any) {
   const {id_ruta, id_unidad, id_fiscal, timestamp_telefono, timestamp_salida} = await request.json();
   console.log(id_ruta, id_unidad, id_fiscal, timestamp_telefono, timestamp_salida);
+       const convertToMinutes = (timeString: string) => {
+         const [time, modifier] = timeString.split(" ");
+         let [hours, minutes] = time.split(":").map(Number);
+         if (modifier === "PM" && hours !== 12) {
+           hours += 12;
+         }
+         if (modifier === "AM" && hours === 12) {
+           hours = 0;
+         }
+         return hours * 60 + minutes;
+       };
   try {
 
     const findFiscal = await fiscales.findOne({ _id: id_fiscal });
@@ -23,6 +34,30 @@ export async function POST(request: any) {
         const saveTimestamp = await timestamp.save();
         return NextResponse.json(saveTimestamp);
       }else{
+        // Obtener la fecha de hoy
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        const tomorrow = new Date(today);
+        tomorrow.setDate(today.getDate() + 1);
+
+        // Buscar registros de timestamps donde coincidan id_unidad, id_ruta y createdAt sea del día de hoy
+        const unidTimeStamps = await timestamps.find({
+          id_unidad,
+          id_ruta,
+          createdAt: {
+            $gte: today,
+            $lt: tomorrow,
+          },
+        });
+
+        // Ordenar unidTimeStamps de más antigua a más reciente, dejando la más reciente en posición 0
+        unidTimeStamps.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+        console.log(unidTimeStamps[0], "Unidad más reciente de la misma ruta");
+
+        // falta comparar tiempos entre los puntos para ofrecer una respuesta diferente en caso de retardo -------------------
         const timestamp = new timestamps({
           id_ruta,
           id_unidad,
