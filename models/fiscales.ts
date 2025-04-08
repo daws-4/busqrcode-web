a4d2790986ae5e1b4e6d87e11d94bd01161730ef
daws-4 <villamizarandresdavid@gmail.com>
2025-04-08 16:27:23 -0400

    08-04-2025

--- models/fiscales.ts
+++ models/fiscales.ts
@@ -3,30 +3,34 @@ import { Schema, model, models } from "mongoose";
 const fiscales = new Schema(
   {
     numero: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    ubicacion: {   
-        type: String,
-        required: true,
-        trim: true,
+    ubicacion: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    username:{
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required: true,
-        trim: true,
+    password: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    sethora:{
-        type: Boolean,
-        default: false,
+    sethora: {
+      type: Boolean,
+      default: false,
+    },
+    setdelete: {
+      type: Boolean,
+      default: false,
     },
   },
   {
