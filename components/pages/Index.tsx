85324e72ae6b84968d13c6f35195ee863e0f0054
David <villamizarandresdavid@gmail.com>
2025-04-13 21:46:27 -0400

    xd

--- components/pages/Index.tsx
+++ components/pages/Index.tsx
@@ -186,7 +186,7 @@ export default function Index({
   };
   const formatHour30secs = (dateString: string) => {
     const date = new Date(dateString);
-    date.setSeconds(date.getSeconds() - 30); // Adelantar 30 segundos jasjsjjadas
+    date.setSeconds(date.getSeconds() - 5); // Adelantar 5 segundos jasjsjjadas
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
